3ff0af3b61bed43dddf5297422a1ce8bc4378227
Dhruv <93705090+thedhruvkumar@users.noreply.github.com>
2022-03-17 11:02:31 +0530

    Update main.js

--- main.js
+++ main.js
@@ -1,5 +1,5 @@
 
-const {Client , Intents , Collection, Message,MessageEmbed} = require('discord.js');
+const {Client , Intents , Collection} = require('discord.js');
 const client = new Client({intents: [Intents.FLAGS.GUILDS , Intents.FLAGS.GUILD_MEMBERS]})
 const config = require('./config.json');
 
@@ -7,16 +7,6 @@ client.commands = new Collection();
 client.events = new Collection();
 
 module.exports = client;
-
-
-client.once('ready', () => {
-	console.log('vikibot online');
-
-	client.user.setActivity('with ur mum');
-
-});
-
-
 
 require('./handler/handler.js')(client);
 
