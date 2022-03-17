f2c5cbec3835db11c1c7c8f168fa7e748e6d88a2
Dhruv <93705090+thedhruvkumar@users.noreply.github.com>
2022-03-17 11:26:01 +0530

    Update interactionCreate.js

--- events/interactionCreate.js
+++ events/interactionCreate.js
@@ -5,8 +5,6 @@ const client = require("../main");
 
 client.on( "interactionCreate" ,	async (interaction) =>{
         if (!interaction.isCommand()) return;
-        // if (!interaction.isButton()) return;
-        // if (!interaction.isSelectMenu()) return;
         const command = client.commands.get(interaction.commandName);
         if (!command) return;
 
@@ -28,37 +26,11 @@ client.on( "interactionCreate" ,	async (interaction) =>{
         } catch (error) {
             if (error)
             {
-                const errorEmbed = new MessageEmbed()
-                .setAuthor(interaction.user.tag ,interaction.user.avatarURL() )
-                .setColor("RANDOM")
-                .setThumbnail(interaction.guild.iconURL())
-                .setTimestamp()
-                .addField('USERID' , interaction.user.id , false)
-                .addField("Command Used" , interaction.commandName , false)
-                .addField("Channel Name" , `#${interaction.channel.name}`,false)
-                .addField('Guild Name' , interaction.guild.name , false)
-                .addField('Guild ID' , interaction.guild.id , false)
-                .setTitle("Error Logs")
-                .addField("Error" , `${error}` , false);
-    
-                interaction.client.channels.cache.get('937394269931569303').send({embeds:[errorEmbed]});
-    
                 console.error(error);
             } 
             await interaction.reply({ content: 'There was an error while executing this command!', ephemeral: true });
         }
-        const actionembed = new MessageEmbed()
-        .setAuthor(interaction.user.tag , interaction.user.avatarURL())
-        .setTimestamp()
-        .setColor('RANDOM')
-        .setThumbnail(interaction.guild.iconURL())
-        .addField("UserId" , interaction.user.id , false)
-        .addField("Command Used" , interaction.commandName , false)
-        .addField("Channel Name" , `#${interaction.channel.name}`,false)
-        .addField("Guild Name" , interaction.guild.name,false)
-        .addField('Guild ID' , interaction.guild.id , false);
-        interaction.client.channels.cache.get('937394269931569303').send({embeds:[actionembed]});
-    
+        
     
 
 });
